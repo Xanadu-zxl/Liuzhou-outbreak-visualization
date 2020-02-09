3ef7d7a29913221a151659bc389423b2317ede51
idcjaja <838927992@qq.com>
2020-02-09 14:44:45 +0800

    feat: add maps

--- src/pages/home/components/Chart.vue
+++ src/pages/home/components/Chart.vue
@@ -3,18 +3,28 @@
     <div class="virus-chart">
       <v-chart :options="epidemicTendencyOptions" />
       <v-chart :options="epidemicTendencyOptions" />
+      <v-chart :options="mapOptions" />
     </div>
   </div>
 </template>
 
 <script>
-import Echart from 'vue-echarts'
+import Echarts from 'vue-echarts'
 import 'echarts/lib/chart/line'
+import 'echarts/lib/chart/map'
 import 'echarts/lib/component/tooltip'
+import 'echarts/lib/component/visualMap'
+import Liuzhou from '../../../resources/liuzhou.json'
+import Luobu from '../../../resources/luobu.json'
+import Luorong from '../../../resources/luorong.json'
+
+Echarts.registerMap('LZ', Liuzhou);
+Echarts.registerMap('LB', Luobu);
+Echarts.registerMap('LR', Luorong);
 
 export default {
   components: {
-    'v-chart': Echart
+    'v-chart': Echarts
   },
 
   data() {
@@ -73,6 +83,76 @@ export default {
             symbolSize: 7,
             lineStyle: {width: 2},
             data: [2, 10, 20, 31, 30, 37]
+          }
+        ]
+      },
+      mapOptions: {
+        tooltip: {
+          trigger: 'item',
+          formatter: '{b}<br/>{c}'
+        },
+        toolbox: {
+          show: true,
+          orient: 'vertical',
+          left: 'right',
+          top: 'center',
+          feature: {
+            dataView: {readOnly: false},
+            restore: {},
+            saveAsImage: {}
+          }
+        },
+        visualMap: {
+          min: 1,
+          max: 20,
+          type: 'piecewise',
+          realtime: false,
+          calculable: true,
+          inRange: {
+            color: ['#ffaa85', '#660309']
+          }
+        },
+        series: [
+          {
+            name: '柳州',
+            type: 'map',
+            mapType: 'LZ',
+            label: {
+              show: false
+            },
+            data: [
+              {name: '三江侗族自治县', value: 0},
+              {name: '融水苗族自治县', value: 0},
+              {name: '融安县', value: 0},
+              {name: '柳城县', value: 0},
+              {name: '鹿寨县', value: 0},
+              {name: '柳北', value: 0},
+              {name: '鱼峰区', value: 0},
+              {name: '柳南区', value: 0},
+              {name: '柳江区', value: 0}
+            ]
+          },
+          {
+            name: '洛埠镇',
+            type: 'map',
+            mapType: 'LB',
+            label: {
+              show: true
+            },
+            data: [
+              {name: '洛埠镇', value: 4}
+            ]
+          },
+          {
+            name: '雒容镇',
+            type: 'map',
+            mapType: 'LR',
+            label: {
+              show: true
+            },
+            data: [
+              {name: '雒容镇', value: 12}
+            ]
           }
         ]
       }
