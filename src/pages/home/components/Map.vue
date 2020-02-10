31e5ea7765443653e08fbdc10f2fa7f243b02fc7
idcjaja <838927992@qq.com>
2020-02-10 10:55:17 +0800

    feat: render map by @byzanteam/map-ui

--- src/pages/home/components/Map.vue
+++ src/pages/home/components/Map.vue
@@ -1,82 +1,80 @@
 <template>
-  <div>
-    <div class="virus-map">
-      <v-chart :options="mapOptions" />
-    </div>
-  </div>
+  <base-map
+    ref="mapRef"
+    :map-options="mapOptions"
+    features="none"
+    :map-style="mapStyle"
+
+  >
+    <regions
+      ref="regionsRef"
+      :areas="areas"
+      :groups="groups"
+      :areaHoverStyle="areaHoverStyle"
+    />
+  </base-map>
 </template>
 
 <script>
-import Echarts from 'vue-echarts'
-import 'echarts/lib/chart/map'
-import 'echarts/lib/component/tooltip'
-import 'echarts/lib/component/visualMap'
+import {
+  BaseMap,
+  Regions,
+} from '@byzanteam/map-ui';
 import Liuzhou from '../../../resources/liuzhou.json'
 
-Echarts.registerMap('LZ', Liuzhou);
 export default {
   components: {
-    'v-chart': Echarts
+    BaseMap,
+    Regions,
   },
 
   data() {
     return {
+      areas: Liuzhou.features,
+      mapStyle: 'amap://styles/cca20692c7b4da0b930eadd919d5a3fb',
       mapOptions: {
-        tooltip: {
-          trigger: 'item',
-          formatter: '{b}<br/>{c}'
-        },
-        toolbox: {
-          show: true,
-          orient: 'vertical',
-          left: 'right',
-          top: 'center',
-          feature: {
-            dataView: {readOnly: false},
-            restore: {},
-            saveAsImage: {}
+        zoom: 10,
+        zoomEnable: false,
+        dragEnable: false,
+        resizeEnable: true,
+        center: [109.603274, 24.398684],
+      },
+      groups: [
+        {
+          name: '雒容镇',
+          codes: [1],
+          style: {
+            fillColor: '#ffaa85',
+            fillOpacity: 0.8,
+            strokeColor: '#ffffff',
+            strokeWeight: 1,
           }
         },
-        visualMap: {
-          min: 1,
-          max: 20,
-          pieces: [
-            {min: 10000},
-            {min: 1000, max: 9999},
-            {min: 100, max: 999},
-            {min: 10, max: 99},
-            {min: 1, max: 9}
-          ],
-          type: 'piecewise',
-          realtime: false,
-          calculable: true,
-          inRange: {
-            color: ['#ffaa85', '#660309']
+        {
+          name: '洛埠镇',
+          codes: [2],
+          style: {
+            fillColor: '#d98066',
+            fillOpacity: 0.8,
+            strokeColor: '#ffffff',
+            strokeWeight: 1,
           }
-        },
-        series: [
-          {
-            name: '柳州',
-            type: 'map',
-            mapType: 'LZ',
-            label: {
-              show: true
-            },
-            data: [
-              {name: '洛埠镇', value: 4},
-              {name: '雒容镇', value: 12}
-            ]
-          }
-        ]
-      }
+        }
+      ],
+      areaHoverStyle: {
+        strokeColor: '#ffffff',
+        strokeWeight: 1,
+        fillColor: '#380206',
+        strokeOpacity: 1,
+        fillOpacity: 0.1,
+      },
     }
   }
 }
 </script>
 
-<style lang="less" scoped>
-.echarts {
-  width: 100%;
-  height: 550px;
+<style scoped>
+.map {
+  height: 500px;
 }
 </style>
