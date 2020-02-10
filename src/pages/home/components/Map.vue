36f1cfb7eed9576e52c7f620eda5d6cec8748d14
idcjaja <838927992@qq.com>
2020-02-10 11:27:59 +0800

    feat: add infowindow

--- src/pages/home/components/Map.vue
+++ src/pages/home/components/Map.vue
@@ -11,6 +11,14 @@
       :areas="areas"
       :groups="groups"
       :areaHoverStyle="areaHoverStyle"
+      @area-clicked="areaClickFunc"
+    />
+    <info-window
+      ref="infowindowRef"
+      :options="{
+        closeWhenClickMap: true,
+        isCustom: true
+      }"
     />
   </base-map>
 </template>
@@ -19,6 +27,7 @@
 import {
   BaseMap,
   Regions,
+  InfoWindow,
 } from '@byzanteam/map-ui';
 import Liuzhou from '../../../resources/liuzhou.json'
 
@@ -26,6 +35,7 @@ export default {
   components: {
     BaseMap,
     Regions,
+    InfoWindow,
   },
 
   data() {
@@ -43,6 +53,7 @@ export default {
         {
           name: '雒容镇',
           codes: [1],
+          center: [109.603274, 24.398684],
           style: {
             fillColor: '#ffaa85',
             fillOpacity: 0.8,
@@ -53,6 +64,7 @@ export default {
         {
           name: '洛埠镇',
           codes: [2],
+          center: [109.513237,24.430415],
           style: {
             fillColor: '#d98066',
             fillOpacity: 0.8,
@@ -69,12 +81,42 @@ export default {
         fillOpacity: 0.1,
       },
     }
+  },
+
+  methods: {
+    areaClickFunc (json, area, vm) {
+      const content = `<div class='info-container'>
+        <div class="info-title">${json.properties.group.name}</div>
+        <div class="info-title">1</div>
+      </div>`
+      this.$refs.infowindowRef.createInfoWindow({content: content, location: json.properties.group.center})
+    }
   }
 }
 </script>
 
-<style scoped>
+<style>
 .map {
   height: 500px;
+}
+
+.info-container {
+  background-color: rgba(31, 36, 64, .6);
+  padding: 6px 8px;
+  color: #ffffff;
+  font-size: 14px;
+  border-radius: 4px;
+  box-shadow: 0 .1rem .1rem 0 transparentize(#000000, .86);
+  position: relative;
+}
+
+.info-container::after {
+  content: "";
+  position: absolute;
+  border: .4rem solid transparent;
+  border-top-color: rgba(31, 36, 64, .6);
+  bottom: -19px;
+  left: 50%;
+  transform: translate(-50%, -50%);
 }
 </style>
