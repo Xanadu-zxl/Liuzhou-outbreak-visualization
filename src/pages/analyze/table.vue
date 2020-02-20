1647de46649cdf60055353a16939a7c31b91ffd7
idcjaja <838927992@qq.com>
2020-02-20 20:14:29 +0800

    feat: add table

---
+++ src/pages/analyze/table.vue
@@ -0,0 +1,39 @@
+<template>
+  <section class="section">
+    <vis-table
+      :headers="tableHeader"
+      :data="data"
+    />
+  </section>
+</template>
+
+<script>
+import { VisTable } from '@byzanteam/vis-components'
+
+export default {
+  components: {
+    VisTable
+  },
+
+  data() {
+    return {
+      tableHeader: [
+        {key: 'name', title: '企业名称'},
+        {key: 'status', title: '防疫物资保障状态'}
+      ],
+      data: [
+        {name: 'xxx公司', status: '可能存在不足'}
+      ]
+    }
+  }
+
+}
+</script>
+
+<style lang="less">
+.table.table--stripe {
+  .table__head {
+    font-size: .7rem !important;
+  }
+}
+</style>
