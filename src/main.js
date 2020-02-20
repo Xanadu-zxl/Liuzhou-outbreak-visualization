0e40fa9c632fc2416e41d2688cde4e63c8abd8d4
idcjaja <838927992@qq.com>
2020-02-20 14:36:43 +0800

    feat: install axios

--- src/main.js
+++ src/main.js
@@ -3,12 +3,16 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import axios from 'axios'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import '@byzanteam/carbonium/dist/carbonium.css'
 
 Vue.use(ElementUI);
 Vue.config.productionTip = false;
+axios.defaults['baseURL'] = 'https://youe.lzdcic.com/v1/components'
+Vue.use(axios)
+
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
