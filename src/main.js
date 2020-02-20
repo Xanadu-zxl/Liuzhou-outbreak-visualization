edb7f4671525a95278dc83726e0ac7bd3864fd77
renhaihuang <790226724@qq.com>
2020-02-20 08:39:27 +0800

    feat: pre set for 2

--- src/main.js
+++ src/main.js
@@ -4,7 +4,10 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import VueI18n from 'vue-i18n'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 
+Vue.use(ElementUI);
 Vue.use(VueI18n);
 Vue.config.productionTip = false;
 const i18n = new VueI18n({
