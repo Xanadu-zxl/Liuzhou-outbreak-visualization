61c623ff6c29ffc5f25debe8863bec3ff76675dd
renhaihuang <790226724@qq.com>
2020-02-21 15:14:52 +0800

    feat: digital 1 page

--- src/pages/analyze/header.vue
+++ src/pages/analyze/header.vue
@@ -7,6 +7,11 @@
       </div>
     </div>
     <div class="digital-roll-container">
+      <div class="digital-roll-next">
+        <img class="digital-roll-next_icon" src="../../assets/img/Icon-Arrow-Right.svg" alt="next-icon">
+      </div>
+      <div class="digital-roll-container--half">
+      <div class="digital-roll-yesterday--top">昨日+{{ companyUpAmount }}人</div>
       <digital-roll
         title-position="bottom"
         :title-style="digital_title"
@@ -14,6 +19,33 @@
         :suffix-style="digital_suffix"
         :content="totalCompany"
       ></digital-roll>
+      <div class="digital-roll-yesterday--bottom">昨日+{{ resumeWorkAmount }}人</div>
+      <digital-roll
+        title-position="bottom"
+        :title-style="digital_title"
+        :digital-style="digital_digital"
+        :suffix-style="digital_suffix"
+        :content="resumeWork"
+      ></digital-roll>
+    </div>
+      <div class="digital-roll-container--half">
+        <div class="digital-roll-yesterday--top">昨日+{{ huBeiProvinceAmount}}人</div>
+        <digital-roll
+          title-position="bottom"
+          :title-style="digital_title"
+          :digital-style="digital_digital"
+          :suffix-style="digital_suffix"
+          :content="huBeiProvince"
+        ></digital-roll>
+        <div class="digital-roll-yesterday--bottom">昨日+{{ inFocusProvinceAmount  }}人</div>
+        <digital-roll
+          title-position="bottom"
+          :title-style="digital_title"
+          :digital-style="digital_digital"
+          :suffix-style="digital_suffix"
+          :content="inFocusProvince"
+        ></digital-roll>
+      </div>
       </div>
   </header>
 </template>
@@ -31,6 +63,25 @@ export default {
         suffix: '个',
         digital: 10
       },
+      companyUpAmount: 1,
+      resumeWork: {
+        title: '准备复工人数',
+        suffix: '人',
+        digital: 10
+      },
+      resumeWorkAmount: 1,
+      huBeiProvince: {
+        title: '湖北籍职工数',
+        suffix: '人',
+        digital: 10
+      },
+      huBeiProvinceAmount: 12,
+      inFocusProvince: {
+        title: '仍在重点防控省份人数',
+        suffix: '人',
+        digital: 10
+      },
+      inFocusProvinceAmount: 12,
       digital_title: {
         color: '#666666',
         fontSize: '0.6rem',
@@ -51,9 +102,21 @@ export default {
   },
   created() {
     axios.get('/51685a1a-e98f-4b9b-b624-dbaf7fa31128/data').then(({data: {data, schema}}) => {
-      console.log('data', data);
-      console.log('schema', schema);
-    })
+      this.totalCompany.digital = data[0][0];
+      this.companyUpAmount = data[0][1]
+    });
+    axios.get('/91b405de-6710-46af-b715-5c51fff851b5/data').then(({data: {data, schema}}) => {
+      this.resumeWork.digital = data[0][0];
+      this.resumeWorkAmount = data[0][1]
+    });
+    axios.get('/3bedea77-6ed7-4c18-99bb-2127aec17101/data').then(({data: {data, schema}}) => {
+      this.huBeiProvince.digital = data[0][0];
+      this.huBeiProvinceAmount = data[0][1]
+    });
+    axios.get('/8db746ce-e83d-4adc-b8a6-a08b4958581c/data').then(({data: {data, schema}}) => {
+      this.inFocusProvince.digital = data[0][0];
+      this.inFocusProvinceAmount = data[0][1]
+    });
   }
 }
 </script>
@@ -148,18 +211,54 @@ export default {
   }
 
 .digital-roll-container {
+  position: relative;
+  display: flex;
   background-color: #fff;
   border-radius: 0.35rem;
   margin: 0.75rem 4% 0.5rem;
   padding: 1.25rem 0.5rem;
 }
-.digital-roll--bottom {
-  display: -webkit-box;
-  display: -ms-flexbox;
+.digital-roll-next {
+  position: absolute;
+  right: 0;
+  top: 50%;
+  transform: translate(50%, -50%);
   display: flex;
-  -webkit-box-orient: vertical;
-  -webkit-box-direction: normal;
-  -ms-flex-direction: column;
-  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 1.25rem;
+  height: 1.25rem;
+  border-radius: .625rem;
+  border: .05rem solid #EAEAEA;
+  background-color: #ffffff;
+  box-shadow: 0 .1rem #00000014;
+}
+
+.digital-roll-next_icon {
+  width: .5rem;
+  height: .5rem;
+}
+.digital-roll-container--half {
+  display: flex;
+  padding-left: 18px;
+  flex-flow: column;
+  width: 50%;
+}
+
+.digital-roll-yesterday--top {
+  margin-top: 1.15rem;
+  margin-bottom: .1rem;
+  color: #666666;
+  font-weight: 400;
+  font-size: 0.55rem;
+  text-align: left;
+}
+.digital-roll-yesterday--bottom {
+  margin-top: 1.5rem;
+  margin-bottom: .1rem;
+  color: #666666;
+  font-weight: 400;
+  font-size: 0.55rem;
+  text-align: left;
 }
 </style>
